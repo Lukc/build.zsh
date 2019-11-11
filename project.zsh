33647817e46546f34266d43efeaf54d3a49e86fc
Marc Chantreux <eiro@phear.org>
2019-11-11 22:27:10 +0100

    be explicit about the fact that a script is a file
    no kidding!  damn it, luka!

--- project.zsh
+++ project.zsh
@@ -3,8 +3,9 @@ package=build_zsh
 version=0.4.1
 
 @script build.zsh
-for it (build/*.zsh)
-    @script $it --install '$(SHAREDIR)/build.zsh'
+local file
+for file (build/*.zsh)
+    @script $file --install '$(SHAREDIR)/build.zsh'
 
 dist=( build/*.zsh build.zsh.in project.zsh Makefile )
 
